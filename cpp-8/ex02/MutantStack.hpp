649cb709be1f78494fcd638e3c4c7b2f6d631c0a
mhoyer <mathieuhoyer@gmail.com>
2023-10-24 17:55:04 +0200

    maj

--- cpp-8/ex02/MutantStack.hpp
+++ cpp-8/ex02/MutantStack.hpp
@@ -1,10 +1,33 @@
 #pragma once
 
+#include <iterator>
 #include <stack>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
 	public:
+		MutantStack() {}
+		MutantStack(MutantStack const &other) : std::stack<T>(other) {}
+
+    	MutantStack &operator=(MutantStack const &other) {
+        	if (this == &other)
+				return *this;
+
+            std::stack<T>::operator=(other);
+        	return *this;
+    	}
+
+		~MutantStack() {}
+
+		typedef typename std::stack<int>::container_type::iterator iterator;
+		iterator begin() {
+			return this->c.begin();
+		}
+		
+		iterator end() {
+			return this->c.end();
+		}
+
 		
 	private:
 
--- cpp-8/ex02/main.cpp
+++ cpp-8/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <iterator>
 
 int main() {
 
@@ -11,8 +12,29 @@ int main() {
 	stack.push(4);
 	stack.push(5);
 
-	std::cout << stack.top() << std::endl;
+	std::cout << std::endl << "Stack utils: " << std::endl;
+	std::cout << "top=" << stack.top() << std::endl;
+	std::cout << "do pop()" << std::endl;
+	stack.pop();
+	std::cout << "top=" << stack.top() << std::endl;
+	std::cout << "do push(10)" << std::endl;
+	stack.push(10);
+	std::cout << "top=" << stack.top() << std::endl;
 
+	std::cout << std::endl << "Iterator: " << std::endl;
+	MutantStack<int>::iterator beg = stack.begin();
+	MutantStack<int>::iterator end = stack.end();
+
+	while (beg != end) {
+		std::cout << *beg << std::endl;
+		++beg;
+	}
+
+	std::cout << std::endl << "Classic printStack: " << std::endl;
+	while (!stack.empty()) {
+        std::cout << stack.top() << std::endl;
+        stack.pop();
+    }
 
 	return 0;
 }
