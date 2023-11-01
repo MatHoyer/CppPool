e3ad4746cd7effd60fe30c7f31b4c03993304ce3
mhoyer <mathieuhoyer@gmail.com>
2023-11-01 11:57:34 +0100

    maj

--- cpp-9/ex02/PmergeMe.cpp
+++ cpp-9/ex02/PmergeMe.cpp
@@ -57,6 +57,7 @@ int jacobsthal(int n) {
         return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
 }
 
+// ========================================== VECTOR ==============================================
 std::vector<std::pair<int, int> > sortPair(std::vector<std::pair<int, int> > S) {
 	std::vector<std::pair<int, int> > sortedS;
 	int min[2];
@@ -101,7 +102,8 @@ std::vector<int> PmergeMe::sortVec() {
 		return _vector;
 	std::vector<std::pair<int, int> > S;
 	for (std::vector<int>::iterator it = _vector.begin(); it != _vector.end(); it++) {
-		std::vector<int>::iterator itNext = it + 1;
+		std::vector<int>::iterator itNext = it;
+		++itNext;
 		if (itNext != _vector.end()) {
 			S.push_back(std::make_pair(*it, *itNext));
 			it++;
@@ -126,6 +128,76 @@ std::vector<int> PmergeMe::sortVec() {
 	return newVector;
 }
 
+// ========================================== LIST ==============================================
+std::list<std::pair<int, int> > sortPair(std::list<std::pair<int, int> > S) {
+	std::list<std::pair<int, int> > sortedS;
+	int min[2];
+	std::list<std::pair<int, int> >::iterator itMem;
+	while (!S.empty()) {
+		min[0] = 0;
+		min[1] = 2147483647;
+		for (std::list<std::pair<int, int> >::iterator it = S.begin(); it != S.end(); it++) {
+			if (it->second < min[1]) {
+				min[0] = it->first; 
+				min[1] = it->second;
+				itMem = it;
+			}
+		}
+		S.erase(itMem);
+		sortedS.push_back(std::make_pair(min[0], min[1]));
+	}
+	return sortedS;
+}
+
+void insertJacob(std::list<std::pair<int, int> > S, std::list<int>& list) {
+	int indJacob = 0;
+	while (indJacob != -1) {
+		int start = jacobsthal(indJacob);
+		if ((size_t)start > list.size())
+			break;
+		std::list<std::pair<int, int> >::iterator it = S.begin();
+		std::advance(it, start);
+		while (!S.empty()) {
+			std::list<int>::iterator insertIt = std::upper_bound(list.begin(), list.end(), it->first);
+			list.insert(insertIt, it->first);
+			S.erase(it);
+			if (it != S.begin())
+				it--;
+		}
+		++indJacob;
+	}
+}
+
+std::list<int> PmergeMe::sortList() {
+	if (_list.empty() || _list.size() == 1)
+		return _list;
+	std::list<std::pair<int, int> > S;
+	for (std::list<int>::iterator it = _list.begin(); it != _list.end(); it++) {
+		std::list<int>::iterator itNext = it;
+		++itNext;
+		if (itNext != _list.end()) {
+			S.push_back(std::make_pair(*it, *itNext));
+			it++;
+		}
+		else
+			S.push_back(std::make_pair(*it, -1));
+	}
+	for (std::list<std::pair<int, int> >::iterator it = S.begin(); it != S.end(); it++) {
+		if (it->first > it->second) {
+			int tmp = it->first;
+			it->first = it->second;
+			it->second = tmp;
+		}
+	}
+	S = sortPair(S);
+	std::list<int> newList;
+	for (std::list<std::pair<int, int> >::iterator it = S.begin(); it != S.end(); it++)
+		newList.push_back(it->second);
+	insertJacob(S, newList);
+	while (newList.front() == -1)
+		newList.erase(newList.begin());
+	return newList;
+}
 
 void PmergeMe::doMerge() {
 	{
@@ -136,11 +208,10 @@ void PmergeMe::doMerge() {
 		printVector(_vector);
 		std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector : " << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
 	}
-	// {
-	// 	clock_t start = clock();
-	// 	_list = sortList(_list);
-	// 	clock_t end = clock();
-	// 	printList(_list);
-	// 	std::cout << "Time to process a range of " << _vector.size() << " elements with std::list : " << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
-	// }
+	{
+		clock_t start = clock();
+		_list = sortList();
+		clock_t end = clock();
+		std::cout << "Time to process a range of " << _list.size() << " elements with std::list : " << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000 << " us" << std::endl;
+	}
 }
--- cpp-9/ex02/PmergeMe.hpp
+++ cpp-9/ex02/PmergeMe.hpp
@@ -18,6 +18,7 @@ class PmergeMe {
 
 
 		std::vector<int> sortVec();
+		std::list<int> sortList();
 		
 		void doMerge();
 
